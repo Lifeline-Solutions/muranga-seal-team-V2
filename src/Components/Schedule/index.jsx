3c563d2551583efaccab3491a96e56c2322551af
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-04-27 15:02:21 +0300

    Completed

--- src/Components/Schedule/index.jsx
+++ src/Components/Schedule/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import wazitologo from "../images/wazitologo.png";
 import msealskit from "../images/msealskit.png";
 import msealslogo from "../images/msealslogo.png";
@@ -17,11 +16,11 @@ const index = () => {
             <p>VS</p>
             <div className="flex flex-col">
               <img src={msealslogo} alt="msealslogo" />
-              <h1>Muranga Seals</h1>
+              <h1>Murang`&apos;`a Seals</h1>
             </div>
           </div>
           <h1 className="text-center w-[70%] mx-auto">
-            Murang'a stadium , Email muranga seals to book a ticket today
+            Murang`&apos;`a stadium , Email muranga seals to book a ticket today
           </h1>
         </div>
 
@@ -34,11 +33,11 @@ const index = () => {
             <p>VS</p>
             <div className="flex flex-col">
               <img src={msealslogo} alt="msealslogo" />
-              <h1>Muranga Seals</h1>
+              <h1>Murang`&apos;`a Seals</h1>
             </div>
           </div>
           <h1 className="text-center w-[70%] mx-auto">
-            Murang'a stadium , Email muranga seals to book a ticket today
+            Murang`&apos;`a stadium , Email muranga seals to book a ticket today
           </h1>
 
           <button className="bg-[#FAE115] text-black font-bold p-2 w-[40%] mx-auto">
