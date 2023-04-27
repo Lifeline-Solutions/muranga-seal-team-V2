f8f3a5a01bc43f5e9f0834b409273182d8615b7b
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-04-27 17:48:32 +0300

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-    <div >
+    <div>
       <NavBar />
       <Hero />
+      <About />
+      <Footer />
     </div>
   );
 };
