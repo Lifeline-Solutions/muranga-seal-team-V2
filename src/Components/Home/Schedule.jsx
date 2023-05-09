6e6b0203e4c93dae9d6c32cd8238a0a8fb0e31f2
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-05-09 13:04:50 +0300

    Home cleanup

--- src/Components/Home/Schedule.jsx
+++ src/Components/Home/Schedule.jsx
@@ -5,55 +5,61 @@ import msealslogo from "../../assets/msealslogo.png";
 const Schedule = () => {
   return (
     <div className="bg-black text-white px-4 py-8 ml-[28px] mr-[28px]">
-      <h1>Match Schedule</h1>
-      <div className="flex justify-between px-4">
+      <h1 className="uppercase">Match Schedule</h1>
+      <div className="flex justify-between ">
         <div className=" text-center border-r  flex flex-col gap-2 border-white">
-          <div className="flex gap-4 justify-center items-center ">
-            <div className="flex  flex-col ">
+          <div className="flex gap-4 mt-8 items-center ">
+            <div className="flex ml-4 mr-12  flex-col ">
               <img src={wazitologo} alt="wazitologo" />
-              <h1>Wazito FC</h1>
+              <h1 className="mt-3">Wazito FC</h1>
             </div>
             <p>VS</p>
-            <div className="flex flex-col">
+            <div className="flex flex-col ml-10">
               <img src={msealslogo} alt="msealslogo" />
-              <h1>Murang&apos;a Seals</h1>
+              <h1 className="mt-3">Murang&apos;a Seals</h1>
             </div>
           </div>
-          <h1 className="text-center w-[70%] mx-auto">
+          <h1 className="text-center w-[70%] mt-5 mx-auto text-[#867A7A]">
             Murang&apos;a stadium , Email muranga seals to book a ticket today
           </h1>
+          <p className="text-[#867A7A]">29/April/2023</p>
+          <button className="bg-[#FAE115] text-black mt-5 font-bold p-2 w-[40%] mx-auto">
+            Buy Ticket
+          </button>
         </div>
 
         <div className=" text-center border-r  flex flex-col gap-2 border-white">
-          <div className="flex gap-4 justify-center items-center ">
-            <div className="flex  flex-col ">
+          <div className="flex gap-4 mt-8 items-center ">
+            <div className="flex ml-4 mr-12  flex-col ">
               <img src={wazitologo} alt="wazitologo" />
-              <h1>Wazito FC</h1>
+              <h1 className="mt-3">Wazito FC</h1>
             </div>
             <p>VS</p>
-            <div className="flex flex-col">
+            <div className="flex flex-col ml-10">
               <img src={msealslogo} alt="msealslogo" />
-              <h1>Murang&apos;a Seals</h1>
+              <h1 className="mt-3">Murang&apos;a Seals</h1>
             </div>
           </div>
-          <h1 className="text-center w-[70%] mx-auto">
+          <h1 className="text-center w-[70%] mt-5 mx-auto text-[#867A7A]">
             Murang&apos;a stadium , Email muranga seals to book a ticket today
           </h1>
-
-          <button className="bg-[#FAE115] text-black font-bold p-2 w-[40%] mx-auto">
+          <p className="text-[#867A7A]">29/April/2023</p>
+          <button className="bg-[#FAE115] text-black mt-5 font-bold p-2 w-[40%] mx-auto">
             Buy Ticket
           </button>
         </div>
 
         <div className="flex justify-between p-4 ">
           <div className="flex flex-col gap-2">
-            <p>AUTHENTIC HOME SHIRT</p>
-            <p>Team jersey</p>
-            <p>1500 KSh</p>
-            <p>Short sleeve</p>
-            <p>10% Discount off On membership</p>
-            <p>Medium</p>
-            <button className="bg-[#FAE115]  flex justify-center text-black font-bold p-2 w-[80%] mx-auto">
+            <p className="mb-4">AUTHENTIC HOME SHIRT</p>
+            <p className="text-[#867A7A] mb-5">Team jersey</p>
+            <p className="mb-4 text-[#867A7A]">1500 KSh</p>
+            <p className="mb-4 text-[#867A7A]">Short sleeve</p>
+            <p className="mb-4 text-[#867A7A]">
+              10% Discount off On membership
+            </p>
+            <p className="mb-5 text-[#867A7A]">Medium</p>
+            <button className="bg-[#FAE115]  flex justify-center text-black font-bold p-2 w-[80%] ">
               Buy Now
             </button>
           </div>
