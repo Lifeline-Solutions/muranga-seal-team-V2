4eab971fb76a61a521b52242f116309ef2e60c06
ger619 <draker74@gmail.com>
2023-06-16 12:41:02 +0300

    Added changes

--- src/Components/Home/Landing.jsx
+++ src/Components/Home/Landing.jsx
@@ -58,11 +58,11 @@ const Landing = () => {
     },
   ];
   return (
-    <div className="flex flex-col md:flex-row gap-5 mt-20  m-4 md:m-[28px]">
-      <div>
+      <div className="flex flex-col md:flex-row gap-5 mt-20  m-4 md:m-[28px]">
+        <div className="w-full">
           <a href="#"
              className=" flex flex-col items-center bg-white border border-[#FAE115] rounded-lg shadow md:flex-row ">
-            <img className="object-cover w-full rounded-t-lg md:h-[400px] md:w-[600px] sm:w-[200px] md:rounded-none md:rounded-l-lg hover:bg-[#FAE115]"
+            <img className="object-cover w-full rounded-t-lg lg:w-[70%] md:h-[400px] md:w-[600px] sm:w-[200px] md:rounded-none md:rounded-l-lg hover:bg-[#FAE115]"
                  src={banner} alt="Muranga seals" />
             <div className="flex flex-col justify-between leading-normal p-5">
               <h5 className="mb-6 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">A Win For Seals</h5>
@@ -75,42 +75,42 @@ const Landing = () => {
           </a>
 
 
-        <div className="flex flex-row  justify-between md:mr-[20px]">
-          {news.map((item) => (
-            <div
-              key={item.id}
-              className="flex flex-col-reverse justify-between mt-5 p-2  group hover:bg-[#FAE115]  hover:scale-105 transition-all duration-500 cursor-pointer"
-              onMouseEnter={changeImage}
-            >
-              <div className="flex-col">
-                <p className="font-bold text-sm">{item.title}</p>
-                <p className="text-sm">{item.description}</p>
-                <p className="bg-[#FAE115] sm:p-2 flex group-hover:bg-black group-hover:text-white justify-center  rounded-lg font-semibold">
-                  Latest News
-                </p>
-              </div>
-              <img className="" src={item.image} alt="" />
-            </div>
-          ))}
+          <div className="flex flex-row  justify-between md:mr-[20px]">
+            {news.map((item) => (
+                <div
+                    key={item.id}
+                    className="flex flex-col-reverse justify-between mt-5 p-2  group hover:bg-[#FAE115]  hover:scale-105 transition-all duration-500 cursor-pointer"
+                    onMouseEnter={changeImage}
+                >
+                  <div className="flex-col">
+                    <p className="font-bold text-sm">{item.title}</p>
+                    <p className="text-sm">{item.description}</p>
+                    <p className="bg-[#FAE115] sm:p-2 flex group-hover:bg-black group-hover:text-white justify-center  rounded-lg font-semibold">
+                      Latest News
+                    </p>
+                  </div>
+                  <img className="" src={item.image} alt="" />
+                </div>
+            ))}
+          </div>
         </div>
-      </div>
 
 
-      <div className="bg-white  flex flex-col md:flex-row border-t-indigo-500">
-        <div className="flex flex-col md:items-end gap-5 hidden sm:flex ">
-          {featuredNews.map((item) => (
-            <div
-              key={item.id}
-              className="flex flex-col md:w-[303px] text-white bg-black"
-            >
-              <img src={item.image} alt="" className="md:w-[300px]" />
-              <p className=" text-xs p-2">Featured</p>
-              <p className="text-xl p-4 font-bold w-[90%]">{item.title}</p>
-            </div>
-          ))}
+        <div className="bg-white  flex flex-col md:flex-row border-t-indigo-500">
+          <div className="flex flex-col md:items-end gap-5 hidden sm:flex ">
+            {featuredNews.map((item) => (
+                <div
+                    key={item.id}
+                    className="flex flex-col md:w-[303px] text-white bg-black"
+                >
+                  <img src={item.image} alt="" className="md:w-[300px]" />
+                  <p className=" text-xs p-2">Featured</p>
+                  <p className="text-xl p-4 font-bold w-[90%]">{item.title}</p>
+                </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
